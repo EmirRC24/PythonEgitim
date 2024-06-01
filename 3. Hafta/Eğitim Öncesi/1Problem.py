f6c3e958fc71dae70d5426eecd8751452e2d1dc1
EmirRC24 <emiririci@gmail.com>
2024-06-01 17:57:27 +0300

    Create 1Problem.py

---
+++ 3. Hafta/Eğitim Öncesi/1Problem.py
@@ -0,0 +1,7 @@
+# 1.Problemin Çözümünü Buraya Yazınız
+sayi = int(input("Bir sayı girin: "))
+
+if sayi % 7 == 0:
+    print(f"{sayi} sayısı 7'ye tam bölünebilir.")
+else:
+    print(f"{sayi} sayısı 7'ye tam bölünemez.")
